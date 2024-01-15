dd5070bc6d6e464972b6c2e2bbb0dbade59a7e70
EhsanRanjoory <154647666+EhsanRanjoory@users.noreply.github.com>
2024-01-15 04:36:21 +0330

    Add files via upload

---
+++ Script/1001search.js
@@ -0,0 +1,19 @@
+﻿// search google by 1001search.ir
+//hint URL: https://www.google.com/search?q=ti+site%253A1001ketab.ir
+
+
+function DoSearch(){
+	var SearchBox = document.getElementById("SearchBox1");
+	var CheckBox = document.getElementById("CheckBox1");
+	if(SearchBox.value == ""){
+		window.alert("متنی را برای جستجو وارد کنید.");
+		return;
+	}
+	URL = "https://www.google.com/search?q=" + SearchBox.value;
+	LimitTo1001ketab = "";
+	if( CheckBox.checked )
+		LimitTo1001ketab = "+site:1001ketab.ir";
+		URL = URL + LimitTo1001ketab;
+	window.location.href = URL;
+}
+    
